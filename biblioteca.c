22d4bf5be0209223a624da67dcd202ea2f15881b
Dan <160802240+mixsz@users.noreply.github.com>
2024-09-20 16:20:50 -0300

    Update biblioteca.c
    funcao "confirmar"

--- biblioteca.c
+++ biblioteca.c
@@ -1,1 +1,17 @@
-//parte das funcoes
+#include "biblioteca.h"
+
+void confirmacao(char *confirmar, char *nome, char *menu, char *sair){ //funcao para continuar no menu ou sair do programa
+  while (confirmar[0] != 's' && confirmar[0] != 'S' && confirmar[0] != 'n' && confirmar[0] != 'N'){
+    printf("\nDeseja continuar? [S/N]: ");
+    fgets(confirmar,sizeof(confirmar),stdin);
+    puts("");
+  }
+  if (confirmar[0] == 's' || confirmar[0] == 'S'){
+    return;
+  }
+  else{
+    printf("Tenha um ótimo dia Sr(a) %s!\n", nome);
+    *menu = 'f';
+    *sair = 't';
+  }
+}
