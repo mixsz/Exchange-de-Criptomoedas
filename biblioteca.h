1095a6d1ee1f57af2f9aff077f1ad0c98f61c353
Caue-Yusuke <cyusuke@gmail.com>
2024-09-28 20:05:49 -0300

    versão anterior
    adição do comprar criptomoedas

--- biblioteca.h
+++ biblioteca.h
@@ -11,18 +11,9 @@
    /*                                       Prototipo das funcoes                                              */
 
 void confirmacao(char *confirmar, char *nome, char *menu, char *sair); //funcao para continuar no menu ou sair do programa
-
 void permissao (char *senharegistrada); // funcao para realizar a acao desejada apos a confirmacao de senha
-
-void consultar_extrato(char *registro); // funcao para consultar o extrato)
-
-void depositar(char *real, char *bitcoin_usuario, char *ripple_usuario, char *ethereum_usuario, char *registro);
-
-void sacar (char *real, char *registro, char *bitcoin_usuario, char *ripple_usuario, char *ethereum_usuario);
-
+void depositar(char *real);
+void sacar (char *real, char *registro);
 void consultar_saldo(char *real,char *bitcoin,char *ripple,char *ethereum);
-
-void comprar_criptomoeda(char *real_usuario, char *bitcoin_usuario, char *ripple_usuario, char *ethereum_usuario, char *registro);
-
-
+void comprar_criptomoeda(char *real_usuario, char *bitcoin_usuario, char *ripple_usuario, char *ethereum_usuario);
 #endif
