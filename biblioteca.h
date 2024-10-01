0d2310be18664f373ff39582b9dd4313d8dfe477
Dan <160802240+mixsz@users.noreply.github.com>
2024-10-01 01:25:50 -0300

    Update biblioteca.h

--- biblioteca.h
+++ biblioteca.h
@@ -13,13 +13,13 @@ void confirmacao(char *confirmar, char *nome, char *menu, char *sair); //funcao 
 
 void permissao (char *senharegistrada); // funcao para realizar a acao desejada apos a confirmacao de senha
 
-void consultar_extrato(char *registro); // funcao para consultar o extrato)
+void consultar_extrato(char *registro, char *nome, char *cpf); // funcao para consultar o extrato)
 
 void depositar(char *real, char *bitcoin_usuario, char *ripple_usuario, char *ethereum_usuario, char *registro);
 
 void sacar (char *real, char *registro, char *bitcoin_usuario, char *ripple_usuario, char *ethereum_usuario);
 
-void consultar_saldo(char *real,char *bitcoin,char *ripple,char *ethereum);
+void consultar_saldo(char *real,char *bitcoin,char *ripple,char *ethereum, char *nome, char *cpf);
 
 void comprar_criptomoeda(char *real_usuario, char *bitcoin_usuario, char *ripple_usuario, char *ethereum_usuario, char *registro, char *cotacao_btc2, char *cotacao_rip2, char *cotacao_eth2);
 
