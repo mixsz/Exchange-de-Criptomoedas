e08ae4b765a733e577b0114f6120b7437525c349
Caue-Yusuke <cyusuke@gmail.com>
2024-09-30 21:29:34 -0300

    Update biblioteca.h

--- biblioteca.h
+++ biblioteca.h
@@ -1,4 +1,3 @@
-
 #ifndef biblioteca
 
 #include <stdio.h>
@@ -11,9 +10,21 @@
    /*                                       Prototipo das funcoes                                              */
 
 void confirmacao(char *confirmar, char *nome, char *menu, char *sair); //funcao para continuar no menu ou sair do programa
+
 void permissao (char *senharegistrada); // funcao para realizar a acao desejada apos a confirmacao de senha
-void depositar(char *real);
-void sacar (char *real, char *registro);
+
+void consultar_extrato(char *registro); // funcao para consultar o extrato)
+
+void depositar(char *real, char *bitcoin_usuario, char *ripple_usuario, char *ethereum_usuario, char *registro);
+
+void sacar (char *real, char *registro, char *bitcoin_usuario, char *ripple_usuario, char *ethereum_usuario);
+
 void consultar_saldo(char *real,char *bitcoin,char *ripple,char *ethereum);
-void comprar_criptomoeda(char *real_usuario, char *bitcoin_usuario, char *ripple_usuario, char *ethereum_usuario);
+
+void comprar_criptomoeda(char *real_usuario, char *bitcoin_usuario, char *ripple_usuario, char *ethereum_usuario, char *registro, char *cotacao_btc2, char *cotacao_rip2, char *cotacao_eth2);
+
+void vender_criptomoeda(char *real_usuario, char *bitcoin_usuario, char *ripple_usuario, char *ethereum_usuario, char *registro, char *cotacao_btc1, char *cotacao_rip1, char *cotacao_eth1);
+
+void atualizar_cotacao(char *cotacao_btc, char *cotacao_rip, char *cotacao_eth);
+
 #endif
